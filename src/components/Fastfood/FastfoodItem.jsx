4a8dfeddd803cd5c8a3f06abc28b690762412e69
kozhushna <k.kozhushna@gmail.com>
2023-10-16 12:22:15 +0300

    Add picture sourse

--- src/components/Fastfood/FastfoodItem.jsx
+++ src/components/Fastfood/FastfoodItem.jsx
@@ -10,7 +10,34 @@ const FastfoodItem = ({
     currency
   }
 }) => {
-  return <h3>{name}</h3>;
+  return (
+    <>
+      <h3>{name}</h3>
+      <picture>
+        <source
+          srcSet={`https://res.cloudinary.com/dm3dq4juf/image/upload/v1697007040/WorkKava/fastfood/${image}.png 1x,
+                        https://res.cloudinary.com/dm3dq4juf/image/upload/v1697007040/WorkKava/fastfood/${image}@2x.png 2x`}
+          media="(min-width: 1440px)"
+        />
+        <source
+          srcSet={`https://res.cloudinary.com/dm3dq4juf/image/upload/v1697007040/WorkKava/fastfood/${image}-table.png 1x,
+                        https://res.cloudinary.com/dm3dq4juf/image/upload/v1697007040/WorkKava/fastfood/${image}-table@2x.png 2x`}
+          media="(min-width: 1024x)"
+        />
+        <source
+          srcSet={`https://res.cloudinary.com/dm3dq4juf/image/upload/v1697007040/WorkKava/fastfood/${image}-mobil.png 1x,
+                        https://res.cloudinary.com/dm3dq4juf/image/upload/v1697007040/WorkKava/fastfood/${image}-mobil@2x.png 2x`}
+          media="(max-width: 1024px)"
+        />
+
+        <img
+          title={title}
+          src={`https://res.cloudinary.com/dm3dq4juf/image/upload/v1697007040/WorkKava/fastfood/${image}.png`}
+          alt={name}
+        />
+      </picture>
+    </>
+  );
 };
 
 export default FastfoodItem;
