0896a5119da8550cd08b5de5bf6e11054d29d377
kozhushna <k.kozhushna@gmail.com>
2023-10-17 12:16:17 +0300

    Add modal

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,23 +1,32 @@
 import { lazy } from 'react';
 import { Navigate, Route, Routes } from 'react-router';
 import { Layout } from './Layout';
+import { Modal } from './Modal/Modal';
 
 const MainPage = lazy(() => import('pages/Main/Main.jsx'));
 const CafePage = lazy(() => import('pages/Cafe/Cafe.jsx'));
 const FastfoodPage = lazy(() => import('pages/Fastfood/Fastfood.jsx'));
 const CoworkingPage = lazy(() => import('pages/Coworking/Coworking.jsx'));
 
+// const App = () => {
+//   return (
+//     <Routes>
+//       <Route path="/" element={<Layout />}>
+//         <Route index element={<MainPage />} />
+//         <Route path="/cafe" element={<CafePage />} />
+//         <Route path="/fastfood" element={<FastfoodPage />} />
+//         <Route path="/coworking" element={<CoworkingPage />} />
+//         <Route path="*" element={<Navigate to="/" />} />
+//       </Route>
+//     </Routes>
+//   );
+// };
+
 const App = () => {
   return (
-    <Routes>
-      <Route path="/" element={<Layout />}>
-        <Route index element={<MainPage />} />
-        <Route path="/cafe" element={<CafePage />} />
-        <Route path="/fastfood" element={<FastfoodPage />} />
-        <Route path="/coworking" element={<CoworkingPage />} />
-        <Route path="*" element={<Navigate to="/" />} />
-      </Route>
-    </Routes>
+    <>
+      <Modal></Modal>
+    </>
   );
 };
 
---
+++ src/components/Modal/Modal.styled.js
@@ -0,0 +1,26 @@
+import styled from '@emotion/styled';
+
+export const Backdrop = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100vw;
+  height: 100vh;
+  background-color: ${props => props.theme.colors.backdrop};
+  z-index: 6;
+  box-sizing: border-box;
+`;
+
+export const ModalContent = styled.div`
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  width: calc(100vw - 48px);
+  background: ${props => props.theme.colors.mainBackground};
+  transform: translate(-50%, -50%);
+  border-radius: 15px;
+
+  @media screen and (${props => props.theme.devices.mobile}) {
+    width: 311px;
+  }
+`;
