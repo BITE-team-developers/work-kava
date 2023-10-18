73310dc8b318ae3fa1689386283976c42eadafbe
Валентина Сіра <113180300+ValentynaSira1@users.noreply.github.com>
2023-10-18 15:13:08 +0300

    add logo

---
+++ src/components/Footer/components/Logo/Logo.styled.js
@@ -0,0 +1,11 @@
+import styled from '@emotion/styled';
+import { NavLink } from 'react-router-dom';
+
+export const LogoWrapper = styled.div`
+    margin-bottom: 50px;
+` 
+
+export const Link = styled(NavLink)`
+    display: flex;
+`
+
