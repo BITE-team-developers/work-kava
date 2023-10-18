fd71a69b23df9b4f0d155ffb287c829fcdc7a40b
Pavlo <113455386+sps-paska-it@users.noreply.github.com>
2023-10-19 01:33:37 +0200

    update header, not finish

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,36 +1,56 @@
 import { Link } from 'react-router-dom';
-import { Item, List, StyledHeader, StyledNav, Wrapper } from './Header.styled';
+import {
+  ContainerHeader,
+  Item,
+  LinkItem,
+  LinkPhone,
+  List,
+  StyledHeader,
+  Wrapper
+  // WrapperPhone
+} from './Header.styled';
+import { Svg } from 'components/SvgIcon/SvgIcon';
 
 export const Header = () => {
   const flot = window.innerWidth;
+  const handleOpenModal = () => {
+    console.log('modal');
+  };
   return (
     <StyledHeader>
-      <p href="#" lang="en">
-        Work kava
-      </p>
-      {flot > 1044 ? (
-        <Wrapper>
-          <StyledNav>
-            <List>
-              <Item key="1">
-                <Link>Home</Link>
-              </Item>
-              <Item key="2">
-                <Link>Cafe</Link>
-              </Item>
-              <Item key="3">
-                <Link>Fastfood</Link>
-              </Item>
-              <Item key="4">
-                <Link>Coworking</Link>
-              </Item>
-            </List>
-          </StyledNav>
-          <p href="tel:+380970000000">097-000-00-00</p>
-        </Wrapper>
-      ) : (
-        <p>button</p>
-      )}
+      <ContainerHeader>
+        <Link to="/">
+          <Svg w={201} h={46} icon={'logo'} style={{ fill: '#fff' }} />
+        </Link>
+        {flot > 1044 ? (
+          <Wrapper>
+            <nav>
+              <List>
+                <Item key="1">
+                  <LinkItem to="/">Home</LinkItem>
+                </Item>
+                <Item key="2">
+                  <LinkItem to="/cafe">Cafe</LinkItem>
+                </Item>
+                <Item key="3">
+                  <LinkItem to="/fastfood">Fastfood</LinkItem>
+                </Item>
+                <Item key="4">
+                  <LinkItem to="/coworking">Coworking</LinkItem>
+                </Item>
+              </List>
+            </nav>
+            <LinkPhone href="tel:+380970000000">
+              <Svg w={32} h={32} icon={'phone'} style={{ fill: '#fff' }} />
+              <p>097-000-00-00</p>
+            </LinkPhone>
+          </Wrapper>
+        ) : (
+          <button type="button" onClick={handleOpenModal}>
+            <Svg w={201} h={46} icon={'burger'} style={{ fill: '#fff' }} />
+          </button>
+        )}
+      </ContainerHeader>
     </StyledHeader>
   );
 };
--- src/components/Header/Header.styled.jsx
+++ src/components/Header/Header.styled.jsx
@@ -1,45 +1,79 @@
 import styled from '@emotion/styled';
+import { Container } from 'components/App.styled';
 import { NavLink } from 'react-router-dom';
 
 export const StyledHeader = styled.header`
   position: fixed;
-  top: 0;
-  left: 0;
+  /* top: 0;
+  left: 0; */
 
   display: flex;
-  justify-content: space-between;
   align-items: center;
   width: 100%;
   height: 80px;
-  padding: 0 138px;
   z-index: 5;
   background-color: ${props => props.theme.colors.headerBackground};
 `;
+
+export const ContainerHeader = styled(Container)`
+  display: flex;
+  justify-content: space-between;
+
+  align-items: center;
+`;
+
 export const Wrapper = styled.div`
   display: flex;
   justify-content: space-between;
-  gap: 118px;
+  align-items: center;
+  column-gap: 118px;
 `;
-
-export const StyledNav = styled.nav``;
 
 export const List = styled.ul`
   display: flex;
-  justify-content: space-between;
 
-  gap: 40px;
+  column-gap: 40px;
 `;
 
 export const Item = styled.li`
+  display: flex;
+  align-items: center;
+
   height: 56px;
   /* border-bottom: 3px solid ${props => props.theme.colors.accent}; */
 `;
 
-export const Link = styled(NavLink)`
+export const LinkItem = styled(NavLink)`
+  position: relative;
+  display: flex;
+  align-items: center;
   height: 56px;
+
+  color: ${props => props.theme.colors.white};
+  font-size: 24px;
 
   &.active {
     color: ${props => props.theme.colors.accent};
-    border-bottom: 3px solid ${props => props.theme.colors.accent};
+
+    &::before {
+      content: '';
+      position: absolute;
+      left: 0;
+      bottom: 0;
+
+      width: 100%;
+      border-radius: 2px;
+      border-bottom: 4px solid ${props => props.theme.colors.accent};
+    }
   }
+`;
+
+export const LinkPhone = styled(NavLink)`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  column-gap: 10px;
+
+  color: ${props => props.theme.colors.white};
+  font-size: 24px;
 `;
