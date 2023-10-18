251bcd56bd852fc125169bffcbc2f834614ee5a9
RomaBortnik <bortnik25051998@gmail.com>
2023-10-18 17:37:06 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,25 +2,22 @@ import { lazy } from 'react';
 import { Navigate, Route, Routes } from 'react-router';
 import { Layout } from './Layout';
 
-// const MainPage = lazy(() => import('pages/Main/Main.jsx'));
-// const CafePage = lazy(() => import('pages/Cafe/Cafe.jsx'));
-// const FastfoodPage = lazy(() => import('pages/Fastfood/Fastfood.jsx'));
-// const CoworkingPage = lazy(() => import('pages/Coworking/Coworking.jsx'));
-
-import Main from 'pages/Main/Main';
+const MainPage = lazy(() => import('pages/Main/Main.jsx'));
+const CafePage = lazy(() => import('pages/Cafe/Cafe.jsx'));
+const FastfoodPage = lazy(() => import('pages/Fastfood/Fastfood.jsx'));
+const CoworkingPage = lazy(() => import('pages/Coworking/Coworking.jsx'));
 
 const App = () => {
   return (
-    // <Routes>
-    //   <Route path="/" element={<Layout />}>
-    //     <Route index element={<MainPage />} />
-    //     <Route path="/cafe" element={<CafePage />} />
-    //     <Route path="/fastfood" element={<FastfoodPage />} />
-    //     <Route path="/coworking" element={<CoworkingPage />} />
-    //     <Route path="*" element={<Navigate to="/" />} />
-    //   </Route>
-    // </Routes>
-    <Main></Main>
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<MainPage />} />
+        <Route path="/cafe" element={<CafePage />} />
+        <Route path="/fastfood" element={<FastfoodPage />} />
+        <Route path="/coworking" element={<CoworkingPage />} />
+        <Route path="*" element={<Navigate to="/" />} />
+      </Route>
+    </Routes>
   );
 };
 
