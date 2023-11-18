005e926daf11b1738e77b76d675b7b39e3f53edf
BITE-team-fs <147430857+BITE-team-developers@users.noreply.github.com>
2023-11-18 20:44:51 +0200

    fix images sizes

--- src/components/HomePages/HomePagePlase.jsx
+++ src/components/HomePages/HomePagePlase.jsx
@@ -25,11 +25,15 @@ export const HomePagePlase = ({ title, description, styles, linkTo }) => {
               srcSet={`https://res.cloudinary.com/dm3dq4juf/image/upload/v1697884261/WorkKava/home-webp/${imageName}-desktop.webp 1x,
                         https://res.cloudinary.com/dm3dq4juf/image/upload/v1697884261/WorkKava/home-webp/${imageName}-desktop@2x.webp 2x`}
               type="image/webp"
+              width={818}
+              height={540}
               media="(min-width: 1440px)"
             />
             <source
               srcSet={`https://res.cloudinary.com/dm3dq4juf/image/upload/v1697884261/WorkKava/home-webp/${imageName}-desktop.jpg 1x,
                         https://res.cloudinary.com/dm3dq4juf/image/upload/v1697884261/WorkKava/home-webp/${imageName}-desktop@2x.jpg 2x`}
+              width={818}
+              height={540}
               media="(min-width: 1440px)"
             />
 
@@ -37,11 +41,15 @@ export const HomePagePlase = ({ title, description, styles, linkTo }) => {
               srcSet={`https://res.cloudinary.com/dm3dq4juf/image/upload/v1697884261/WorkKava/home-webp/${imageName}-tablet.webp 1x,
                         https://res.cloudinary.com/dm3dq4juf/image/upload/v1697884261/WorkKava/home-webp/${imageName}-tablet@2x.webp 2x`}
               type="image/webp"
+              width={516}
+              height={480}
               media="(min-width: 1024px)"
             />
             <source
               srcSet={`https://res.cloudinary.com/dm3dq4juf/image/upload/v1697884261/WorkKava/home-webp/${imageName}-tablet.jpg 1x,
                         https://res.cloudinary.com/dm3dq4juf/image/upload/v1697884261/WorkKava/home-webp/${imageName}-tablet@2x.jpg 2x`}
+              width={516}
+              height={480}
               media="(min-width: 1024px)"
             />
 
@@ -49,17 +57,23 @@ export const HomePagePlase = ({ title, description, styles, linkTo }) => {
               srcSet={`https://res.cloudinary.com/dm3dq4juf/image/upload/v1697884261/WorkKava/home-webp/${imageName}-mobile.webp 1x,
                         https://res.cloudinary.com/dm3dq4juf/image/upload/v1697884261/WorkKava/home-webp/${imageName}-mobile@2x.webp 2x`}
               type="image/webp"
+              width={356}
+              height={218}
               media="(max-width: 1023px)"
             />
             <source
               srcSet={`https://res.cloudinary.com/dm3dq4juf/image/upload/v1697884261/WorkKava/home-webp/${imageName}-mobile.jpg 1x,
                         https://res.cloudinary.com/dm3dq4juf/image/upload/v1697884261/WorkKava/home-webp/${imageName}-mobile@2x.jpg 2x`}
               type="image/webp"
+              width={356}
+              height={218}
               media="(max-width: 1023px)"
             />
 
             <CoffeeImg
               styles={styles}
+              width={356}
+              height={218}
               src={`https://res.cloudinary.com/dm3dq4juf/image/upload/v1697884261/WorkKava/home-webp/${imageName}-mobile.jpg`}
               alt={title}
               onError={e => {
